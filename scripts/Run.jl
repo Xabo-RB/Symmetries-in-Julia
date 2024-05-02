c58e3291a2c3b25cf84a2965cbd21b560c143b39
Xabier Rey Barreiro <xabo.rey@gmail.com>
2024-05-02 11:21:42 +0200

    Update Run.jl

--- scripts/Run.jl
+++ scripts/Run.jl
@@ -73,6 +73,28 @@ ecuaciones = [
 ]
 
 #_________________________________________________________________________#
+# ________________________ Example 4.3 SEIR __________________________
+
+@variables t
+
+states = ["S", "E", "I", "R", "Q"]
+
+salidas = 1
+
+parameters = ["beta","v","psi","gamma"]
+
+inputs = []
+
+ecuaciones = [
+    "-beta*S*I",
+    "beta*S*I - v*E",
+    "v*E - psi*I -(1-psi)*gamma*I",
+    "gamma*Q +(1-psi)*gamma*I",
+    "-gamma*Q + psi*I",
+    "Q"
+]
+
+#_________________________________________________________________________#
 
 CreateModel = userDefined(states,salidas,parameters,inputs,ecuaciones)
 
