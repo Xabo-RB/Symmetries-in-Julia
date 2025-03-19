7fe4b482ead274ad762deeb72a7aa4a489f8fc18
Xabier Rey Barreiro <xabo.rey@gmail.com>
2025-03-19 11:17:22 +0100

    Update Run_ContSyms.jl

--- scripts/Run_ContSyms.jl
+++ scripts/Run_ContSyms.jl
@@ -5,5 +5,11 @@ using Latexify
 
 @quickactivate "Symmetries in Julia"
 
-include("Model.jl")
+include("Model.jl"); CreateModel
 
+# Reconocer todas las variables del modelo como strings a variables
+
+stringEstados = CreateModel.states;
+stringParametros = CreateModel.parameters;
+stringEntradas = CreateModel.inputs;
+stringEcuaciones = CreateModel.ecuaciones;
