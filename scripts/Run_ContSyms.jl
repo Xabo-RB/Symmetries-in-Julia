acfd35981649761723640abf74d803319cac3268
Xabier Rey Barreiro <xabo.rey@gmail.com>
2025-03-19 13:02:04 +0100

    n

--- scripts/Run_ContSyms.jl
+++ scripts/Run_ContSyms.jl
@@ -5,12 +5,24 @@ using Latexify
 
 @quickactivate "Symmetries in Julia"
 
-include("Model.jl"); CreateModel
+include("Model.jl"); 
 
 # Reconocer todas las variables del modelo como strings a variables
 
-stringEstados = CreateModel.states;
-stringParametros = CreateModel.parameters;
-stringEntradas = CreateModel.inputs;
+stringEstados = CreateModel.estados;
+stringParametros = CreateModel.parametros;
+stringEntradas = CreateModel.entradas;
 stringEcuaciones = CreateModel.ecuaciones;
 
+# Crear variables simbólicas para los estados, parámetros, entradas y ecuaciones
+
+states = ["x1", "x2", "x3", "x4"]
+
+# Diccionario con clave = nombre (string), valor = la variable simbólica
+States_dict = Dict{String, Symbolics.Var}()
+for s in states
+    States_dict[s] = Symbolics.Var(Symbol(s))
+end
+
+@show var_dict["x1"]
+@show var_dict["x2"]
