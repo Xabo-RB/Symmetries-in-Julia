fc121471e7d8050f8f1da3990c507410df3810f7
Xabier Rey Barreiro <xabo.rey@gmail.com>
2025-05-13 13:07:45 +0200

    n

--- scripts/LieSyms.jl
+++ scripts/LieSyms.jl
@@ -10,6 +10,8 @@ using LaTeXStrings
 include("Model.jl"); 
 include("functions_conts.jl")
 include(srcdir("convertToMaple.jl"))
+include(srcdir("convertToLatex.jl"))
+
 
 struct symbolic_variables
 
@@ -51,75 +53,20 @@ whatIs = 0
 (epsi_syms, psiJ, Jg) = funcion4ta(symbols, whatIs)
 
 (states_Obs, outputs_Obs) = observabilityContinous(symbols, epsiJg, dgdx, epsi_syms)
+fullSystemObs = vcat(states_Obs,outputs_Obs)
 
 # Convertir cada Num a su cadena LaTeX
-n = length(states_Obs)
+n = length(fullSystemObs)
 latex_strings = Vector{String}(undef, n)
 latex_custom = Vector{String}(undef, n)
 for i in 1:n
     # latexify devuelve un objeto LatexString, lo convertimos a String
-    ls = latexify(states_Obs[i])
-    latex_strings[i] = string(ls)
+    ls = latexify(fullSystemObs[i])
+    latex_strings[i] = string(ls* " = 0")
     # raw"\epsilon" inserta literal \epsilon en la cadena
     latex_custom[i] = replace(latex_strings[i], "epsi" => raw"\xi")
     render(LaTeXString(latex_custom[i]))
 end
 
-convertToMaple(states_Obs, name, 0)
+convertToMaple(fullSystemObs, name, 0)
 convertToLatex(latex_custom, name, 0)
-
-#==
-# 1) Tus datos de Model.jl
-stringEstados    = CreateModel.estados
-stringParametros = CreateModel.parametros
-stringEntradas   = CreateModel.entradas
-stringEcuaciones = CreateModel.ecuaciones[1:4]
-
-# 2) Declarar todas las variables simbólicas a la vez usando el macro @variables y evaluando la cadena generada
-decl = "@variables " *
-       join(stringEstados,   " ") * " " *
-       join(stringParametros," ") * " " *
-       join(stringEntradas,  " ")
-
-decl_dx = decl * " " * join([ "d" * s for s in stringEstados ], " ")
-eval(Meta.parse(decl_dx))  # Convierte el string en una expresión Julia y la evalúa para declarar las variables simbólicas
-
-
-# 3) Ahora reconstruyo mis vectores con esos mismos objetos Num:
-state_syms = Num[]
-for s in stringEstados
-    simb = Symbol(s)         # Convertir el string a símbolo Julia (:x1, :x2, etc.)
-    obj  = eval(simb)        # Evaluar el símbolo, que ya ha sido definido como variable simbólica
-    push!(state_syms, obj)   # Añadir el objeto simbólico al vector
-end
-param_syms = Num[]
-for p in stringParametros
-    simb = Symbol(p)
-    obj  = eval(simb)
-    push!(param_syms, obj)
-end
-input_syms = Num[]
-for u in stringEntradas
-    simb = Symbol(u)
-    obj  = eval(simb)
-    push!(input_syms, obj)
-end
-
-Dstate_syms = [ eval(Symbol("d"*s))   for s in stringEstados ]
-
-# 4) Parseo y evalúo cada ecuación (ya usan exactamente esos x1,x2,…):
-symbolic_expressions = []
-for eq in stringEcuaciones
-    parsed_expr = Meta.parse(eq)  # Convertir el string en una expresión Julia (Expr)
-    symbolic_eq = eval(parsed_expr)  # Evaluar esa expresión usando las variables simbólicas declaradas
-    push!(symbolic_expressions, symbolic_eq)  # Añadir la expresión al vector de expresiones simbólicas
-end
-
-# 4) gᵢ = dxi – fi 
-g = Vector{typeof(Dstate_syms[1])}()
-for i in eachindex(Dstate_syms, symbolic_expressions)
-    push!(g, Dstate_syms[i] - symbolic_expressions[i])
-end
-
-Jg = Symbolics.jacobian(g, state_syms)
-==#
